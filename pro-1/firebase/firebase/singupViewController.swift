394b862b4069ede9f9d775a7212706f2931007fd
VachhaniMeet <43877405+VachhaniMeet@users.noreply.github.com>
2023-10-25 11:54:52 +0530

    Add files via upload

---
+++ pro-1/firebase/firebase/singupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  singupViewController.swift
+//  firebase
+//
+//  Created by MAC1 on 25/10/23.
+//
+
+import UIKit
+
+class singupViewController: UIViewController {
+
+    @IBOutlet weak var txt_username: UITextField!
+    @IBOutlet weak var txt_password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signup(_ sender: Any)
+    {
+        if txt_username.text == ""
+        {
+            
+        }
+    }
+    
+}
