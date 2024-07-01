fa2207e7e6bf08ec5e52110bc5420ad9b57a4845
VachhaniMeet <43877405+VachhaniMeet@users.noreply.github.com>
2024-07-01 12:05:20 +0530

    Add files via upload

---
+++ pincode/pincode/homeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  homeViewController.swift
+//  pincode
+//
+//  Created by MAC2 on 10/04/1946 Saka.
+//
+
+import UIKit
+
+class homeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func segment(_ sender: UISegmentedControl)
+    {
+        
+    }
+    
+}
